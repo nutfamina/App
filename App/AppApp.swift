bfa37a18f8f1d1502276da927bf56ee08185579b
nutfamina <nutfamina@mail.ru>
2025-07-22 19:35:31 +0300

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Amina Nutfullina on 22.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
