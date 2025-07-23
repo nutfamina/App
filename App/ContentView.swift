d9f16c5711cf931067a9845eac178d369070d770
krnklvx <sawosit@gmail.com>
2025-07-23 20:46:12 +0300

    поменяла файлу

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Artur!")
+            Text("Hello!")
         }
         .padding()
     }
