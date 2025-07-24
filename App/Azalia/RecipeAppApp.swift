e64a77b64135b8a1dac25b0df8dead5f09ab834b
Azaliya <azaliya.pochta@gmail.com>
2025-07-24 23:14:10 +0300

    закомментила

--- App/Azalia/RecipeAppApp.swift
+++ App/Azalia/RecipeAppApp.swift
@@ -5,16 +5,16 @@
 //  Created by Азалия Хайруллина on 22.07.2025.
 //
 
-import SwiftUI
-
-@main //точка входа в приложении
-struct RecipeAppApp: App {
-    var body: some Scene {
-        WindowGroup {
-            //заглушка, потом меняем на TabBarView()
-            NavigationStack {
-                RecipeDetailView(recipe: RecipeData.recipes.first!)
-            }
-        }
-    }
-}
+//import SwiftUI
+//
+//@main //точка входа в приложении
+//struct RecipeAppApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            //заглушка, потом меняем на TabBarView()
+//            NavigationStack {
+//                RecipeDetailView(recipe: RecipeData.recipes.first!)
+//            }
+//        }
+//    }
+//}
