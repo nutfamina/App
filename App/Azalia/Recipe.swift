e73fb3e20713a982efb54dc99eca339e042de636
Azaliya <azaliya.pochta@gmail.com>
2025-07-24 03:27:47 +0300

    add recipe and ingredient
    я добавила структуры
    recipe - id, name, imageName, calories, time, ingredients, steps
    и ingredient - id, name, amountPerServing, unit.

---
+++ App/Azalia/Recipe.swift
@@ -0,0 +1,26 @@
+//
+//  Recipe.swift
+//  RecipeApp
+//
+//  Created by Азалия Хайруллина on 22.07.2025.
+//
+
+import Foundation
+
+struct Recipe: Identifiable {
+    let id = UUID()
+    var name: String
+    var imageName: String
+    var calories: Int
+    var time: Int
+    var ingredients: [Ingredient]
+    var steps: [String]
+}
+
+struct Ingredient: Identifiable {
+    let id = UUID()
+    var name: String
+    var amountPerServing: Double
+    var unit: String
+}
+
